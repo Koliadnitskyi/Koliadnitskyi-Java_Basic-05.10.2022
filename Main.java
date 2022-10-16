e2b89fb2c55f329759da64a71dcf70f69d20175b
Koliadnitskyi Valerii <0164zeos20081@gmail.com>
2022-10-16 11:24:11 +0300

    ДЗ 4 версия 3.0 после Ctrl + Alt + L

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        String  team1 = "Мужчины";
+        String team1 = "Мужчины";
         int playerOfTeam1DimaNumberOfFrags = 34;
         int playerOfTeam1VictorNumberOfFrags = 12;
         int playerOfTeam1ValeryNumberOfFrags = 7;
         int playerOfTeam1KirilNumberOfFrags = 22;
         int playerOfTeam1VladNumberOfFrags = 14;
 
-        String  team2 = "Женщины";
+        String team2 = "Женщины";
         int playerOfTeam2TatianaNumberOfFrags = 35;
         int playerOfTeam2IrinaNumberOfFrags = 14;
         int playerOfTeam2KaterinaNumberOfFrags = 4;
@@ -27,7 +27,8 @@ public class Main {
             System.out.println("Победила команда " + team1 + " набравшая " + arithmeticMeanPointsteam1 + " очков.");
         } else if (arithmeticMeanPointsteam1 < arithmeticMeanPointsteam2) {
             System.out.println("Победила команда " + team2 + " набравшая " + arithmeticMeanPointsteam2 + " очков.");
-        } else {System.out.println("Команда " + team1 + " и команда " + team2 + " набрали одинаковое количество очков. Ничья");
+        } else {
+            System.out.println("Команда " + team1 + " и команда " + team2 + " набрали одинаковое количество очков. Ничья");
         } // Вареант ничьи некто не отбрасывал, добавел его сюда, так как в случае ничьи вообще нечего не вывело в консоль
 
         /*
