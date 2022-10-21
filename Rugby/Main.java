d432f7a4f0df924a533d276020a01a1f4d5b26bc
Koliadnitskyi Valerii <0164zeos20081@gmail.com>
2022-10-22 00:41:37 +0300

    Рэгби-команды

--- Rugby/Main.java
+++ Rugby/Main.java
@@ -4,18 +4,18 @@ public class Main {
         int[] teamTwoRugby = new int[25];
 
         System.out.println("Возраст каждого играка первой команды регби:");
-        int teamPlayerNumberCounterOne = 1;
+        int counterForNumbersGamerTeamsOne = 1;
         for (int i = 0; i < teamOneRugby.length; i++) {
             int a = (int) (18 + Math.random() * 22);
             teamOneRugby[i] = a;
-            System.out.println("Возраст играка № " + teamPlayerNumberCounterOne++ + " = " + teamOneRugby[i] + " лет.");
+            System.out.println("Возраст играка № " + counterForNumbersGamerTeamsOne++ + " = " + teamOneRugby[i] + " лет.");
         }
         System.out.println("Возраст каждого играка второй команды регби:");
-        int teamPlayerNumberCounterTwo = 1;
+        int counterForNumbersGamerTeamsTwo = 1;
         for (int g = 0; g < teamTwoRugby.length; g++) {
             int a = (int) (18 + Math.random() * 22);
             teamTwoRugby[g] = a;
-            System.out.println("Возраст играка № " + teamPlayerNumberCounterTwo++ + " = " + teamTwoRugby[g] + " лет.");
+            System.out.println("Возраст играка № " + counterForNumbersGamerTeamsTwo++ + " = " + teamTwoRugby[g] + " лет.");
         }
         double averageAgeTeamOneRugby = 0;
         for (int name : teamOneRugby) {
