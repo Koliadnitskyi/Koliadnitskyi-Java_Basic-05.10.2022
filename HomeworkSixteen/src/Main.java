8ac86ec160e0b2567f3d4e3833a468351438b950
Valery Kolyadnitsky <javabasic29.09.2022group1@gmail.com>
2022-11-19 17:06:35 +0200

    Fourteen+Fifteen+Sixteen

--- HomeworkSixteen/src/Main.java
+++ HomeworkSixteen/src/Main.java
@@ -1,21 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        // ↓ Создаём экземпляр (объект) класса Androids
         Androids myAndroids = new Androids("маленький", "белый");
-        LinuxOS.printModelISamsung();
-        myAndroids.checkingForAGuarantee();
-        myAndroids.checkingForMms();
-        myAndroids.call();
-        myAndroids.sms();
-        myAndroids.internet();
 
-        System.out.print("\n");
+        LinuxOS.printModelISamsung(); // Вызываем метод printModelIPhone у interface Smartphones
+        myAndroids.checkingForAGuarantee(); // Вызываем метод checkingForAGuarantee у класса Androids
+        myAndroids.checkingForMms(); // Вызываем метод checkingForMms у класса Androids
+        myAndroids.call(); // Вызываем метод call у класса Androids
+        myAndroids.sms(); // Вызываем метод sms у класса Androids
+        myAndroids.internet(); // Вызываем метод internet у класса Androids
 
+        System.out.print("\n");// Делаем отступы между о
+
+        // ↓ Создаём экземпляр (объект) класса Iphones
         Iphones myIphones = new Iphones("большой", "чёрный");
-        Ios.printModelIPhone();
-        myIphones.checkingForAGuarantee();
-        myIphones.checkingForMms();
-        myIphones.call();
-        myIphones.sms();
-        myIphones.internet();
+
+        Ios.printModelIPhone(); // Вызываем метод printModelIPhone у interface Smartphones
+        myIphones.checkingForAGuarantee(); // Вызываем метод checkingForAGuarantee у interface Iphones
+        myIphones.checkingForMms(); // Вызываем метод checkingForMms у interface Iphones
+        myIphones.call(); // Вызываем метод call у interface Iphones
+        myIphones.sms(); // Вызываем метод sms у interface Iphones
+        myIphones.internet(); // Вызываем метод internet у interface Iphones
     }
 }
